89eb8fe82d7ff557005b6f67c233a7a492106cdc
kek119 <dozywer@gmail.com>
2024-10-25 18:33:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,24 @@
-   document.querySelector('.menu-button').addEventListener('click', () => {
-    document.querySelector('.menu').classList.toggle('active');
-});
-
-
-
-
-
-
-
-
 document.addEventListener('DOMContentLoaded', function() {
     const button = document.getElementById('toggle-size-btn');
     let isReduced = false;
 
     button.addEventListener('click', function() {
         if (!isReduced) {
-
-            
-
             document.querySelector('body').style.fontSize = '0.9em'; 
             document.body.style.transform = 'scale(0.9)'; 
-
             button.textContent = 'Повернути розмір елементів'; 
             isReduced = true; 
         } else {
-
-            
-
             document.querySelector('body').style.fontSize = ''; 
             document.body.style.transform = ''; 
-
             button.textContent = 'Зменшити розмір елементів'; 
             isReduced = false; 
         }
     });
 });
+document.querySelector('.menu-button').addEventListener('click', () => {
+    document.querySelector('.menu').classList.toggle('active');
+});
+
 
 
